2e4ea8bbd15db2d81d5106a56d3a0695f59cdc8b
Hezi Henen <ezequielhenen@hotmail.com>
2021-05-24 00:06:13 +0300

    24_05_2021 - heroku

--- app.js
+++ app.js
@@ -16,20 +16,40 @@ mongoose
   .then(() => console.log("Connected to MongoDB..."))
   .catch((err) => console.error("Could not connect to MongoDB..."));
 
-app.use(cors()); // to put on comment before upload to a real server
-app.use(express.json());
+// ** MIDDLEWARE ** //
+const whitelist = [
+  "http://localhost:3000",
+  "http://localhost:8181",
+  "https://shrouded-journey-38552.herokuapp.com",
+];
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin);
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable");
+      callback(null, true);
+    } else {
+      console.log("Origin rejected");
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
 
-//Serve Static files from react
-app.use(express.static(path.join(__dirname, "../real-app/public")));
+app.use(cors(corsOptions)); // to put on comment before upload to a real server
+app.use(express.json());
 
 app.use("/api/users", users);
 app.use("/api/auth", auth);
 app.use("/api/cards", cards);
 
-//The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../real-app/public/index.html"));
-});
+if (process.env.NODE_ENV === "production") {
+  //Serve Static files from react
+  app.use(express.static(path.join(__dirname, "../real-app/build")));
+  //The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../real-app/build", "index.html"));
+  });
+}
 
-const port = 8181;
+const port = process.env.PORT || 8181;
 http.listen(port, () => console.log(`Listening on port ${port}...`));
